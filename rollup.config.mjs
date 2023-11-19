0a971733fc2e013aaa7e2230c1e678e6f5a54308
Brandons404 <brandon531@gmail.com>
2023-11-19 14:24:55 -0600

    removing sourcemaps again

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -16,7 +16,6 @@ export default [
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true,
       },
     ],
     plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
